302066b902be317b4e2927d2603d591a4583b574
CatsPoo <ron.gorlik@gmail.com>
2018-03-27 23:11:27 +0300

    add data structure

---
+++ LooPortalBK/base_serials.js
@@ -0,0 +1,40 @@
+module.exports = function () {
+    return {
+        base111: {
+            name: 'Kiria',
+            routers: 
+            [
+                {
+                    name: 'D1',
+                    address: '127.0.0.1',
+                    serials: 
+                    [
+                        {
+                            name: 'se0',
+                            isActive: true
+                        },
+                        {
+                            name: 'se1',
+                            isActive: true
+                        }
+                    ]
+                },
+                {
+                    name: 'D2',
+                    address: '127.0.0.1',
+                    serials:
+                    [
+                        {
+                            name: 'se0',
+                            isActive: true
+                        },
+                        {
+                            name: 'se1',
+                            isActive: true
+                        }
+                    ]
+                }
+            ]
+        }
+    }
+}
--- LooPortalBK/routes/index.js
+++ LooPortalBK/routes/index.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var snmpReq=require('../snmp_client');
+var bases=require('../base_serials');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
-  console.log();
-  snmpReq.send("127.0.0.1","public","1.3.6.1.2.1.2.2.1.2.2",(data)=>{
+var base111=bases().base111;
+  snmpReq.send(base111.routers[0].address,"public","1.3.6.1.2.1.2.2.1.2.2",(data)=>{
     res.render('index', { title: 'Express',res: data});
   });
 });
